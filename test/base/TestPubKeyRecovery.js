dbc8ca945b99fc6e003fcc191fd2f602e0e4027b
Sangmoon Oh <halfface@chollian.net>
2021-03-09 16:06:22 +0900

    Updating test cases on public key recovery for both Python(web3.py) and JavaScript(web3.js).

--- test/base/TestPubKeyRecovery.js
+++ test/base/TestPubKeyRecovery.js
@@ -7,6 +7,23 @@ const {keccak256, keccak256s} = require('eth-lib/lib/hash');
 const elliptic = require('elliptic');
 const secp256k1 = new (elliptic.ec)('secp256k1');
 
+
+const recoverPublicKey = (message, signature) => {
+
+  const msgBytes = web3.utils.hexToBytes(web3.utils.utf8ToHex(message));
+  const preamble = '\x19Ethereum Signed Message:\n' + msgBytes.length;
+  const msg191 = keccak256s(Buffer.concat([Buffer.from(preamble), Buffer.from(msgBytes)]));
+
+  const vals = Account.decodeSignature(signature);
+  const vrs = {v: Bytes.toNumber(vals[0]), r:vals[1].slice(2), s:vals[2].slice(2)};
+  const pubkey = '0x' + secp256k1.recoverPubKey(
+      new Buffer(msg191.slice(2), "hex"), vrs, vrs.v < 2 ? vrs.v : 1 - (vrs.v % 2))
+    .encode('hex', false).slice(2); 
+      
+  return pubkey
+}
+
+
 contract("Test public key recovery from the signature.", async accounts => {
   
   'use strict';
@@ -16,23 +33,22 @@ contract("Test public key recovery from the signature.", async accounts => {
   // References
   //   - https://github.com/ChainSafe/web3.js/blob/v1.3.4/packages/web3-eth-accounts/src/index.js#L335
   //   - https://github.com/MaiaVictor/eth-lib/blob/master/src/account.js
-  it("", async() => {
+  it("Can recover public key from a message and its signature.", async() => {
     
-    const prvkey = '0x052fdb8f5af8f2e4ef5c935bcacf1338ad0d8abe30f45f0137943ac72f1bba1e';
-    const pubkey = '0x' + secp256k1.keyFromPrivate(
-      new Buffer(prvkey.slice(2), "hex")).getPublic(false, "hex").slice(2);
-    const addr = '0x' + keccak256(pubkey).slice(-40);
-    
-    console.log(`private key : ${prvkey}`);
-    console.log(`public key  : ${pubkey}`);
-    console.log(`address     : ${addr}`);
+    const sk = '0x052fdb8f5af8f2e4ef5c935bcacf1338ad0d8abe30f45f0137943ac72f1bba1e';
+    const pk = '0x' + secp256k1.keyFromPrivate(
+      new Buffer(sk.slice(2), "hex")).getPublic(false, "hex").slice(2);
+    const addr = '0x' + keccak256(pk).slice(-40);
     
     assert.equal(addr.toUpperCase(), accounts[0].toUpperCase());
+    console.log(`private key : ${sk}`);
+    console.log(`public key  : ${pk}`);
+    console.log(`address     : ${addr}`);
     
     const msg = 'We built this city.';
     const sig = await web3.eth.sign(msg, accounts[0]);
     console.log(`message     : ${msg}`);
-    console.log(`signature   : ${sig}`);
+    console.log(`ethereum signature   : ${sig}`);
     
     // recover address (not public key) using `web3.eth.accounts.recover()` function
     const addrRe = await web3.eth.accounts.recover(msg, sig);
@@ -49,13 +65,41 @@ contract("Test public key recovery from the signature.", async accounts => {
     // eth-lib/account/recover
     const vals = Account.decodeSignature(sig);
     const vrs = {v: Bytes.toNumber(vals[0]), r:vals[1].slice(2), s:vals[2].slice(2)};
-    const pubkeyRe = '0x' + secp256k1.recoverPubKey(
+    const pkRe = '0x' + secp256k1.recoverPubKey(
         new Buffer(msg191.slice(2), "hex"), vrs, vrs.v < 2 ? vrs.v : 1 - (vrs.v % 2))
       .encode('hex', false).slice(2); 
     
-    assert.equal(pubkeyRe.toUpperCase(), pubkey.toUpperCase());
+    assert.equal(pkRe.toUpperCase(), pk.toUpperCase());
     console.log(`public key recovered from signature : 
-                ${pubkeyRe} - same with the above value`);
+                ${pkRe} - same with the above value`);
   })
+  
+  
+  it("Can recover public key from a message and its signature.", async() => {
+
+    const sk = '0x052fdb8f5af8f2e4ef5c935bcacf1338ad0d8abe30f45f0137943ac72f1bba1e';
+    const pk = '0x' + secp256k1.keyFromPrivate(
+      new Buffer(sk.slice(2), "hex")).getPublic(false, "hex").slice(2);
+    const addr = '0x' + keccak256(pk).slice(-40);
+    
+    assert.equal(addr.toUpperCase(), accounts[0].toUpperCase());
+    console.log(`public key : ${pk}`)
+    
+    const test = async(msg) => {
+      const sig = await web3.eth.sign(msg, accounts[0]);
+      console.log(`message              : ${msg}`);
+      console.log(`ethereum signature   : ${sig}`);
+    
+      const pkRe = recoverPublicKey(msg, sig)
+      assert.equal(pk, pkRe);
+      console.log(`recovered public key : ${pkRe}`);
+    };
+    
+    await test('Life is Live');
+    await test('내 마음 깊은 곳의 너');
+    await test('12345^&*()');      
+    
+  })  
+
   
 })
--- test/base/test_recover.py
+++ test/base/test_recover.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python3
 
+from typing import Union
+from hexbytes import HexBytes
 from eth_keys import keys
 from eth_account import Account
 from eth_account.messages import encode_defunct
 from eth_account._utils.signing import to_standard_signature_bytes
 from eth_utils.curried import keccak
 from eth_typing import Hash32
+
+def recover_pubkey(message : str, signature: Union[str, HexBytes]) -> HexBytes:
+  
+  if isinstance(signature, str) :
+    sig = HexBytes.fromhex(signature[2::] if signature.startswith('0x') else signature)
+  else :
+    sig = signature
+  
+  msg_191 = keccak(
+    text='\x19Ethereum Signed Message:\n' + str(len(message)) + message)
+  sig_std = to_standard_signature_bytes(sig)
+  
+  # recovering public key of secp256k1 keypair using `eth_keys` package
+  pk_re = keys.Signature(sig_std).recover_public_key_from_msg_hash(Hash32(msg_191))
+  
+  print(f'recovered public key : {pk_re.to_hex()}')
+
+  return HexBytes.fromhex(pk_re.to_hex()[2::])
+
+
 
 def test_hello():
   print("Hello !")
@@ -20,25 +42,24 @@ def test_hello():
 #   - EIP-191 Signed Data Standard : https://eips.ethereum.org/EIPS/eip-191  
 def test_recover_pubkey(accounts, web3):
   
-  
   # private key of accounts[0] - configured to local ganache-cli
-  prvkey = keys.PrivateKey(bytes.fromhex(
+  sk = keys.PrivateKey(bytes.fromhex(
     '0x052fdb8f5af8f2e4ef5c935bcacf1338ad0d8abe30f45f0137943ac72f1bba1e'[2::]))
-  print(f'privat key : {prvkey.to_hex()}')
-  print(f'public key : {prvkey.public_key.to_hex()}')
-  print(f'address    : {prvkey.public_key.to_address()}')
+  print(f'privat key : {sk.to_hex()}')
+  print(f'public key : {sk.public_key.to_hex()}')
+  print(f'address    : {sk.public_key.to_address()}')
   
-  assert prvkey.public_key.to_address().upper() == accounts[0].address.upper()
+  assert sk.public_key.to_address().upper() == accounts[0].address.upper()
   
   msg = 'We built this city.'
   sig = web3.eth.sign(accounts[0].address, text=msg) # HexBytes
   print(f'message    : {msg}')
-  print(f'signature  : {sig.hex()}')
+  print(f'ethereum signature  : {sig.hex()}')
   
   # recover address (not public key) using `eth_account.account.Account.recover_message`
   addr_re=Account.recover_message(encode_defunct(text=msg), signature=sig)
   
-  assert addr_re.upper() == prvkey.public_key.to_address().upper()
+  assert addr_re.upper() == sk.public_key.to_address().upper()
   print(f'address recovered from signature : {addr_re} - same with the above value')
   
   # EIP-191 (https://eips.ethereum.org/EIPS/eip-191) defines preproessing 
@@ -48,10 +69,33 @@ def test_recover_pubkey(accounts, web3):
   sig_std = to_standard_signature_bytes(sig)
   
   # recovering public key of secp256k1 keypair using `eth_keys` package
-  pubkey_re = keys.Signature(sig_std).recover_public_key_from_msg_hash(Hash32(msg_191))
+  pk_re = keys.Signature(sig_std).recover_public_key_from_msg_hash(Hash32(msg_191))
   
-  assert pubkey_re.to_hex().upper() == prvkey.public_key.to_hex().upper()
+  assert pk_re.to_hex().upper() == sk.public_key.to_hex().upper()
   print(f'''public key recovered from signature : 
-            {pubkey_re.to_hex()} - same with the above value''')
+            {pk_re.to_hex()} - same with the above value''')
 
+
+  
+def test_recover_pubkey_function(accounts, web3):
+
+  sk = keys.PrivateKey(bytes.fromhex(
+    '0x052fdb8f5af8f2e4ef5c935bcacf1338ad0d8abe30f45f0137943ac72f1bba1e'[2::]))
+  print(f'public key : {sk.public_key.to_hex()}')
+
+
+  def test(msg):
+    sig = web3.eth.sign(accounts[0].address, text=msg)
+    print('\n')
+    print(f'message              : {msg}')
+    print(f'ethereum signature   : {sig}')
+
+    pk = recover_pubkey(msg, sig)
+    
+    assert pk.hex() == sk.public_key.to_hex()
+    print(f'recovered public key : {pk.hex()}')
+    
+  test('Life is Live')
+  test('12345^&*()     abcdeFGHIJ')
+  test('내 마음 깊은 곳의 너')
   
