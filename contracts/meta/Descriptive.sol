1dc7ea1e8bc03790c92fbc463e30c28ed879f721
Sangmoon Oh <halfface@chollian.net>
2021-04-15 18:42:04 +0900

    ...

--- contracts/meta/Descriptive.sol
+++ contracts/meta/Descriptive.sol
@@ -14,80 +14,115 @@ struct Attribute{
     string value;
 }
 
+// @TODO Who can change attributes ?
 
 contract Descriptive{
     using EnumerableSet for EnumerableSet.UintSet;
     using Math for uint256;
 
-    Attribute[] private attribs;
-    mapping(string => EnumerableSet.UintSet) private attribsByName; // attrib name => attribs
-    
-    
+    Attribute[] private _attribs;
+    mapping(string => EnumerableSet.UintSet) private _idxsByName; // attrib name => attrib indexes
+    EnumerableSet.UintSet private _firstIdxs;  // attrib indexes on first values for each name
 
-    string private name;
+    string private _name;
     
     event AttributeSet(string indexed name, string value, uint indexed index);
+    event AttributeAdded(string indexed name, string value, uint count, uint indexed index);
     
     function getName() public view returns (string memory){
-        return name;
+        return _name;
     }
     
-    function setName(string memory _name) public{
-        name = _name;
+    function setName(string memory name) public{
+        _name = name;
     }
     
-    function getAttribute(string memory _name) public view returns (string memory){
-        uint n = attribsByName[_name].length();
+    function getAttribute(string memory name) public view returns (string memory){
+        uint m = _idxsByName[name].length();
         string memory val;
-        if(n > 0) val = attribs[attribsByName[_name].at(0)].value;
+        if(m > 0) val = _attribs[_idxsByName[name].at(0)].value;
         
         return val;
     }
     
-    function getAttributes(string memory _name) public view returns (string[] memory){
-
-        uint n = attribsByName[_name].length();
-        string[] memory vals = new string[](n);
+    function getAttributes(string memory name) public view returns (string[] memory){
+        uint m = _idxsByName[name].length();
+        string[] memory vals = new string[](m);
         
-        for(uint i = 0; i < n; i++){
-            vals[i] = attribs[attribsByName[_name].at(i)].value;
+        for(uint i = 0; i < m; i++){
+            vals[i] = _attribs[_idxsByName[name].at(i)].value;
         }
         
         return vals;
     }
     
-    function setAttribute(string memory _name, string memory _value) public{
-        uint n = attribsByName[_name].length();
-        for(uint i = 0; i < n; i++){
-            delete attribs[attribsByName[_name].at(i)];
+    function setAttribute(string memory name, string memory value) public{
+        uint m = _idxsByName[name].length();
+        if(m > 0){
+            _firstIdxs.remove(_idxsByName[name].at(0));
+            for(uint i = 0; i < m; i++){
+                delete _attribs[_idxsByName[name].at(i)];
+            }
         }
 
-        delete attribsByName[_name];
-        uint idx = attribs.length;
-        attribsByName[_name].add(idx);
-        attribs.push(Attribute(_name, _value));
-        
-        emit AttributeSet(_name, _value, idx);
+        delete _idxsByName[name];
+
+        uint n = _attribs.length;
+        _attribs.push(Attribute(name, value));
+        _idxsByName[name].add(n);
+        _firstIdxs.add(n);
+
+        emit AttributeSet(name, value, n);
     }
     
-    function addAttribute(string memory _name, string memory _value) public{
-
+    function addAttribute(string memory name, string memory value) public{
+        uint n = _attribs.length;
+        _attribs.push(Attribute(name, value));
+        _idxsByName[name].add(n);
+        if(_idxsByName[name].length() == 1) _firstIdxs.add(n);
+        
+        emit AttributeAdded(name, value, _idxsByName[name].length(), n);
     }
     
     function getAttributeNames() public view returns (string[] memory){
-        
+        uint l = _firstIdxs.length();
+
+        string[] memory names = new string[](l);
+        for(uint i = 0; i < l; i++){
+            names[i] = _attribs[_firstIdxs.at(i)].name;
+        }
+
+        return names;
     }
     
-    function removeAttribute(string memory _name, string memory _value) public{
+    function removeAttribute(string memory name, string memory value) public{
+        uint m = _idxsByName[name].length();
+        
+        if(m == 0) return;
+        
+        bytes32 hash = keccak256(abi.encodePacked(value));
+        string memory val;
+        uint idx;
+        for(uint i = 0; i < m; i++){
+            idx = _idxsByName[name].at(i);
+            val = _attribs[idx].value;
+            if(keccak256(abi.encodePacked(val)) == hash){
+                delete _attribs[idx];
+                _idxsByName[name].remove(idx);
+                if(_firstIdxs.contains(idx)){
+                    _firstIdxs.remove(idx);
+                    if(_idxsByName[name].length() > 0){
+                        _firstIdxs.add(_idxsByName[name].at(0));
+                    }
+                }
+            }
+        }
         
     }
     
     function removeAttributes(string memory _name) public{
         
     }
-    
-    
 
-    
 }
 
--- test/meta/Descriptive_test.py
+++ test/meta/Descriptive_test.py
@@ -12,7 +12,7 @@ from brownie import accounts, Descriptive
 
 KEYS = ['color', 'height', 'width', 'weight', 'max', 'min', 'country', 'city']
 COLORS = ['Black', 'White', 'Gray', 'Red', 'Green', 'Blue', 'Orange', 'Brown', 'Orange']
-
+COUNTRIES = ['kr', 'us', 'jp', 'au', 'be', 'br', 'ca', 'cn', 'dk', 'fr']
 
 @pytest.fixture
 def deployer():
@@ -31,7 +31,7 @@ def test_inital_state(testee):
     assert testee.getAttribute(key) == ''
   
 
-def test_set_attribute_singular(testee):
+def test_set_attribute(testee):
   color = random.choice(COLORS)
   testee.setAttribute("color", color)
 
@@ -44,4 +44,36 @@ def test_set_attribute_singular(testee):
   assert values[0] == color
   print(values)
   print(type(values))
+  
+
+def test_add_attribute(testee):
+  countries = random.choices(COUNTRIES, k=3)
+  
+  for cnt in countries:
+    testee.addAttribute("countries", cnt)
+    
+  values = testee.getAttributes("countries")
+  assert len(values) == 3
+  for val in values:
+    assert val in countries 
+  print(values)
+  
+
+def test_get_attribute_names(testee):
+  keys = ['color', 'width', 'height', 'weight', 'countries']
+  
+  testee.setAttribute(keys[0], random.choice(COLORS))
+  testee.setAttribute(keys[1], "30")
+  testee.setAttribute(keys[2], "40")
+  testee.setAttribute(keys[3], "100")
+  testee.addAttribute(keys[4], COUNTRIES[0])
+  testee.addAttribute(keys[4], COUNTRIES[1])
+  
+  names = testee.getAttributeNames()
+  assert len(names) == 5
+  for nm in names:
+    assert nm in keys
+  print(names)
+  
+  
   
