69f056b3ccce2243e278c8d923095e2c2e90916d
Sangmoon Oh <halfface@chollian.net>
2023-05-22 15:24:24 +0900

    Commit again.

---
+++ hardhat.config.ts
@@ -0,0 +1,29 @@
+
+
+// https://hardhat.org/hardhat-runner/docs/config
+const config: HardhatUserConfig = {
+
+  paths: {
+    artifacts : "./build/hardhat",
+    tests: "./hardhat/test",
+    cache: "./hardhat/cache"
+  },
+
+  // https://www.npmjs.com/package/solc?activeTab=versions
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19"
+      },
+      {
+        version: "0.7.6"
+      },
+      {
+        version: "0.6.12"
+      }
+    ],
+  }
+
+};
+
+export default config;
